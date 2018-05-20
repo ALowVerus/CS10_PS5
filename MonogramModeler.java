6d6d80bfa69539f9831e16b4dd2a8aaa9b2e69e6
alowverus <aidan.low98@gmail.com>
2018-05-20 15:55:06 -0400

    blah

--- MonogramModeler.java
+++ MonogramModeler.java
@@ -40,21 +40,7 @@ public class MonogramModeler {
 			// Inputs word, get out HashMap of POS with # of times used as POS
 			HashMap<String, HashMap<String, Double>> POSWords = new HashMap<String, HashMap<String, Double>>(); 
 			// Inputs POS, gets # of times it transitions to other POS
-			AdjacencyMapGraph<String, Double> POSTransitions = new AdjacencyMapGraph<String, Double>(); 
-			
-			// Put all the parts of speech we need into a list.
-			ArrayList<String> POSList = new ArrayList<String>(
-					Arrays.asList(
-							"ADJ", "ADV", "CNJ", "DET", "EX", "FW", "MOD", 
-							"N", "NP", "NUM", "PRO", "P", "TO", "UH", 
-							"V", "VD", "VG", "VN", "WH", ".", sentenceStarter,
-							",", "``", "VBZ", "''", "*", ":", "(", ")", "--", "'", "NIL" // for the brown corpus
-					)
-			);
-			// Add all parts of speech as objects into the graph.
-			for (String POS : POSList) {
-				POSTransitions.insertVertex(POS);
-			}
+			HashMap<String, Double> POSUsage = new HashMap<String, Double>(); 
 			
 			// READ ALL WORDS INTO PARTS OF SPEECH MAP.
 			// While there is another line, read it.
@@ -64,7 +50,6 @@ public class MonogramModeler {
 				splitTagLine = tagLine.split(" ");
 				splitSentenceLine = sentenceLine.split(" ");
 				// Iterate through the split lines.
-				String currentTag = sentenceStarter;
 				for(int i = 0; i < splitTagLine.length; i++) {
 					// Get your next tag and word.
 					String nextTag = splitTagLine[i];
@@ -75,25 +60,12 @@ public class MonogramModeler {
 					if (POSWord == null) {
 						POSWord = new HashMap<String,Double>();
 					}
-					if (POSWord.containsKey(nextTag)) {
-						Double numberOfTimesThatThisWordHasBeenUsedAsThisPOS = POSWord.get(nextTag);
-						POSWord.put(nextTag, numberOfTimesThatThisWordHasBeenUsedAsThisPOS + 1);
-					}
-					else {
-						POSWord.put(nextTag, 1.0);
-					}
+					if (POSWord.containsKey(nextTag)) { POSWord.put(nextTag, POSWord.get(nextTag) + 1); }
+					else { POSWord.put(nextTag, 1.0); }
 					POSWords.put(nextWord, POSWord);
 					// Get the number of times POS1 has gone to POS2
-					Double n;
-					if (POSTransitions.getLabel(currentTag, nextTag) == null) { n = 1.0;}
-					else { 
-						n = POSTransitions.getLabel(currentTag, nextTag) + 1.0; 
-						POSTransitions.removeDirected(currentTag, nextTag);
-					}
-					// Insert a directed edge with 1 more than before
-					POSTransitions.insertDirected(currentTag, nextTag, n);
-					// Reset currentTag to nextTag
-					currentTag = nextTag;
+					if (POSUsage.get(nextTag) == null) { POSUsage.put(nextTag, 1.0); }
+					else { POSUsage.put(nextTag, POSUsage.get(nextTag) + 1.0); }
 				}
 			}
 			// Close files.
@@ -106,38 +78,22 @@ public class MonogramModeler {
 			 * CONVERTING THE MAP AND ADJACENCY GRAPH TO PERCENTAGE HITS.
 			 */
 			
-			// Change the POSTransitions graph from using rote numbers to percentage hits.
-			Iterator<String> vertexIterator = POSTransitions.vertices().iterator();
-			Iterator<String> neighborIterator;
-			while(vertexIterator.hasNext()) {
-				String currentVertex = vertexIterator.next();
-				// Iterate through neighbors of current vertex, get the total number of hits
-				neighborIterator = POSTransitions.outNeighbors(currentVertex).iterator();
-				Double currentValue, sum = 0.0;
-				while (neighborIterator.hasNext()) {
-					String neighborVertex = neighborIterator.next();
-					currentValue = POSTransitions.getLabel(currentVertex, neighborVertex);
-					sum += currentValue;
-				}
-				ArrayList<String> toBeRemoved = new ArrayList<String>();
-				// Iterate through neighbors again, changing routes to percentage hits
-				
-				// MAKE MY FAKE LIST, AND ADD EVERYTHING TO IT
-				neighborIterator = POSTransitions.outNeighbors(currentVertex).iterator();
-				while (neighborIterator.hasNext()) {
-					String neighborVertex = neighborIterator.next();
-					toBeRemoved.add(neighborVertex);
-				}
-				for(String i: toBeRemoved) {
-					Double initialValue = POSTransitions.getLabel(currentVertex, i);
-					POSTransitions.removeDirected(currentVertex, i);
-					POSTransitions.insertDirected(currentVertex, i, Math.log(initialValue/sum));
-				}
+			// Change the POSUsage map from using rote numbers to percentage hits.
+			// Get the sum of hits
+			Double POSsum = 0.0;
+			for (String POS : POSUsage.keySet()) { POSsum += POSUsage.get(POS); }
+			// Turn all hits into percentages, and get most likely
+			String bestPOS = POSUsage.keySet().iterator().next();
+			Double bestScore = POSUsage.get(bestPOS);
+			for (String POS : POSUsage.keySet()) { 
+				// If this POS has a higher number of hits, it is the best
+				if (POSUsage.get(POS) > POSUsage.get(bestPOS)) { bestPOS = POS; }
+				// Log the shit out of this
+				POSUsage.put(POS, Math.log(POSUsage.get(POS) / POSsum)); 
 			}
 			
 			// Change the POSWords map from using rote numbers to percentage hits.
 			for (String word : POSWords.keySet()) {
-				
 				// Get the sum of hits
 				Double sum = 0.0;
 				for (String POS : POSWords.get(word).keySet()) {
@@ -149,104 +105,52 @@ public class MonogramModeler {
 				}
 			}
 			
+			// Get the best POS, for nulls
+			
+			
 			/*
 			 * INTERPRETING TEST FILES.
 			 */
 			
 			System.out.println("TESTING BEGUN");
-			System.out.println(POSTransitions);
 			
 			// Create bufferedReader for testing sentences and bufferedWriter for putting output.
 			BufferedReader testSentences = load(textFolder + textSubject + "-test-sentences.txt");
-			BufferedWriter resultTagsIn = write(textFolder + textSubject + "-result-tags.txt");
+			BufferedWriter resultTagsIn = write(textFolder + textSubject + "-monogram-tags.txt");
 			
-			// Allocate memory to stuff that we need.
-			HashMap<String, Double> currentScores, nextScores;
-			String currentPOS; 
-			HashMap<String, String> thisFrame, nextLayer;
-			ArrayList<HashMap<String, String>> backtraces;
-			Double currentScore, transitionScore, observationScore, nextScore, bestEndValue;
-			Iterator<String> currentScoresIterator, neighborPOSs;
+			String calcTagsString, bestTag, firstTag;
+			Double nextTagScore, bestTagScore;
+			HashMap<String,Double> wordScores;
 			
 			// Keep going until we run out of lines to read.
-			Boolean ranOnce = false;
 			while ((sentenceLine = testSentences.readLine()) != null) {
-				// Create required data structures for testing files, regenerate each line
-				currentScores = new HashMap<String, Double>(); 
-				currentScores.put(sentenceStarter, 0.0);
-				nextScores = new HashMap<String, Double>();
-				backtraces = new ArrayList<HashMap<String, String>>();
 				
+				// Reset tags and sentence
+				calcTagsString = "";
 				// Iterate through each line in the sentence.
 				splitSentenceLine = sentenceLine.split(" ");
 				for (String nextWord : splitSentenceLine) {
-					// Make new backpointer frame for this word.
-					thisFrame = new HashMap<String, String>(); // KEY:VALUE = NEXT_POS:CURRENT_POS
-					nextScores = new HashMap<String, Double>();
-					// Iterate through current scores.
-					currentScoresIterator = currentScores.keySet().iterator();
-					while (currentScoresIterator.hasNext()) {
-						currentPOS = currentScoresIterator.next();
-						// Making a score for the current state to the next state with the next word.
-						currentScore = currentScores.get(currentPOS);
-						neighborPOSs = POSTransitions.outNeighbors(currentPOS).iterator();
-						while (neighborPOSs.hasNext()) {
-							String nextPOS = neighborPOSs.next();
-							transitionScore = POSTransitions.getLabel(currentPOS, nextPOS);
-							// If the word has never been used as this type, give it a -10.0 score.
-							if (POSWords.get(nextWord) == null) {
-								observationScore = -100.0;
-							}
-							else if (POSWords.get(nextWord).get(nextPOS) == null) { 
-								observationScore = -100.0; 
-							}
-							// Else, the word exists and has been used as this type. Give it its proper score.
-							else { observationScore = POSWords.get(nextWord).get(nextPOS); }
-							nextScore = currentScore + transitionScore + observationScore;
-							// If you haven't seen this next state before, put in your score for the next state
-							if (nextScores.get(nextPOS) == null || nextScores.get(nextPOS) <= nextScore) {
-								nextScores.put(nextPOS, nextScore);
-								thisFrame.put(nextPOS, currentPOS);
-							}
+					if (POSWords.get(nextWord) != null) {
+						wordScores = POSWords.get(nextWord);
+					}
+					else {
+						wordScores = new HashMap<String,Double>();
+						for (String POS : POSUsage.keySet()) {
+							wordScores.put(POS, 0.0);
 						}
-						
 					}
-					// Reset states and scores after you've iterated. Clear and put, rather than set=.
-					currentScores.clear();
-					currentScores.putAll(nextScores); // This doesn't trigger ConcurrentModificationException.
-					backtraces.add(thisFrame);
-					ranOnce = true;
-				}
-				
-				// BACKTRACE! Generate array POS, iterate to generate a string, copy string into an output file.
-				
-				// Get the best end POS.
-				bestEndValue = nextScores.get(nextScores.keySet().iterator().next());
-				currentPOS = sentenceStarter;
-				for (String POS : nextScores.keySet()) {
-					if (bestEndValue < nextScores.get(POS)) {
-						bestEndValue = nextScores.get(POS);
-						currentPOS = POS;
+					bestTag = wordScores.keySet().iterator().next();
+					bestTagScore = wordScores.get(bestTag) + POSUsage.get(bestTag);
+					for (String nextTag : wordScores.keySet()) {
+						nextTagScore = wordScores.get(nextTag) + POSUsage.get(nextTag);
+						if (nextTagScore > bestTagScore) {
+							bestTagScore = nextTagScore;
+						}
 					}
-				}
-				
-				// Generate the list.
-				ArrayList<String> backtracedListPOS = new ArrayList<String>();
-				int layersBack = 0;
-				while (currentPOS != sentenceStarter) {
-					backtracedListPOS.add(currentPOS);
-					nextLayer = backtraces.get(backtraces.size() - 1 - layersBack);
-					currentPOS = nextLayer.get(currentPOS);
-					layersBack ++;
-				}
-								
-				// Write generated words to a string.
-				String backtracedStringPOS = "";
-				for (int i = 0; i < backtracedListPOS.size(); i ++) {
-					backtracedStringPOS += backtracedListPOS.get(backtracedListPOS.size() - 1 - i) + " ";
+					calcTagsString += bestTag + " ";
 				}
 				// Write backtraced string into an output file.
-				resultTagsIn.write(backtracedStringPOS + "\n");
+				resultTagsIn.write(calcTagsString + "\n");
 			}
 			
 			// Close testing file and results writer
@@ -259,7 +163,7 @@ public class MonogramModeler {
 			
 			// Open test and result tags
 			BufferedReader testTags = load(textFolder + textSubject + "-test-tags.txt");
-			BufferedReader resultTagsOut = load(textFolder + textSubject + "-result-tags.txt");
+			BufferedReader resultTagsOut = load(textFolder + textSubject + "-monogram-tags.txt");
 			// Run through each line of the test and result tags
 			String testTagsLine, resultTagsOutLine;
 			String[] splitTestTagsLine, splitResultTagsOutLine;
